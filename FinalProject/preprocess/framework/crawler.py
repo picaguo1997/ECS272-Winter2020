43e448988679e386bf5603ae8c49bcf36935a80f
YunHsin Kuo <yhkuo@campus-042-145.ucdavis.edu>
2020-03-10 20:42:27 -0700

    eastern media crawler stuff

--- FinalProject/preprocess/framework/crawler.py
+++ FinalProject/preprocess/framework/crawler.py
@@ -4,7 +4,7 @@ from selectolax.parser import HTMLParser
 
 
 class Crawler(object):
-    
+
     def __init__(self, keywords, max_urls=50):
         self.links = []
         self.keywords = keywords
@@ -15,10 +15,10 @@ class Crawler(object):
         self.internal_urls = set()
         self.external_urls = set()
         self.total_urls_visited = 0
-        
+
     def get_links(self):
         return self.internal_urls
-        
+
     def crawl(self, url):
         """
         Crawls a web page and extracts all links.
@@ -27,32 +27,35 @@ class Crawler(object):
         self.total_urls_visited += 1
         if self.total_urls_visited <= self.max_urls:
             print("crawling", self.total_urls_visited, "/", self.max_urls)
-        
+
         self.links = self.get_all_website_links_selectolax(url)
-        
+
         for link in self.links:
             if self.total_urls_visited > self.max_urls:
             #if len(self.internal_urls) > self.max_urls:
                 break
             self.crawl(link)
-            
-        
-            
+
+
+
     def is_valid(self, url):
         parsed = urlparse(url)
         return bool(parsed.netloc) and bool(parsed.scheme)
-    
+
     def get_all_website_links_selectolax(self, url):
         urls = set()
-        
+
         domain_name = urlparse(url).netloc
-        
+
+
         try:
-            r = urllib.request.urlopen(url)
+            headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+            req = urllib.request.Request(url=url, headers=headers)
+            r = urllib.request.urlopen(req)
             sll =  HTMLParser(r.read())
         except:
             return urls
-        
+
         for a_tag in sll.css("a"):
             if not "href" in a_tag.attributes:
                 continue
@@ -77,8 +80,9 @@ class Crawler(object):
                 continue
 
             a_tag_text = a_tag.text(deep=True, separator='', strip=False)
+            #if not any(word in a_tag_text for word in self.keywords):
             if not (any(word in a_tag_text for word in self.keywords) or any(word in href for word in self.keywords)):
-                #check if A tag text OR A tag href doesn't contain keyword  
+                #check if A tag text OR A tag href doesn't contain keyword
                 continue
 
             urls.add(href)
